c1e2e19d78cfad3b4a318f6e71a76e978cccbfa1
Marco Pacchialat <m.pacchialat@gmail.com>
2023-01-01 16:28:34 +0100

    gaming and movement layers

--- keyboards/ymdk/ymd40/air40/keymaps/w-colemak/keymap.c
+++ keyboards/ymdk/ymd40/air40/keymaps/w-colemak/keymap.c
@@ -20,7 +20,10 @@ enum layer_names {
   _QWERTY,
   _LOWER,
   _RAISE,
-  _ADJUST
+  _ADJUST,
+  _GAMING_LO,
+  _GAMING_HI,
+  _MOVEMENT
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -34,7 +37,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_TILD, KC_BSPC, KC_CIRC, KC_AMPR,    KC_ASTR,    KC_LPRN, KC_RPRN,
         KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_DEL,  KC_PIPE, KC_UNDS,    KC_PLUS,    KC_LCBR, KC_RCBR,
         KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______, S(KC_NUHS), S(KC_NUBS), KC_HOME, KC_END,
-        _______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT,    KC_VOLD,    KC_VOLU, KC_MPLY
+        TO(4),   _______, _______, TO(6),   _______, _______, _______, _______, KC_MNXT,    KC_VOLD,    KC_VOLU, KC_MPLY
     ),
     [_RAISE] = LAYOUT_ortho_4x12(
         KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_GRV,  KC_BSPC, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
@@ -47,6 +50,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, RGB_MOD, RGB_HUD, RGB_SAD, RGB_VAD, AG_NORM, AG_SWAP, _______, KC_LEFT, KC_DOWN, KC_RGHT, _______,
         _______, BL_TOGG, BL_DOWN, BL_UP,   BL_BRTG, _______, _______, _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+    ),
+    [_GAMING_LO] = LAYOUT_ortho_4x12(
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    _______, _______, _______, KC_UP,   _______, DB_TOGG,
+        KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    AG_SWAP, _______, KC_LEFT, KC_DOWN, KC_RGHT, _______,
+        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    _______, _______, _______, _______, _______, _______,
+        TO(0),   _______, _______, KC_TAB,  MO(5),   _______, _______, _______, _______, _______, _______, _______
+    ),
+    [_GAMING_HI] = LAYOUT_ortho_4x12(
+        KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    _______, _______, _______, KC_UP,   _______, DB_TOGG,
+        _______, _______, _______, _______, _______, _______, AG_SWAP, _______, KC_LEFT, KC_DOWN, KC_RGHT, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        TO(0),   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+    ),
+    [_MOVEMENT] = LAYOUT_ortho_4x12(
+        _______, _______, KC_UP,   _______, _______, _______, _______, _______, _______, KC_UP,   _______, DB_TOGG,
+        _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        TO(0),   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
     )
 };
 
