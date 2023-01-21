1a56aa3250a95322aca6caf23c733a3250bd0bf9
Marco Pacchialat <m.pacchialat@gmail.com>
2023-01-21 15:05:30 +0100

    layer without homerow mods

--- keyboards/ymdk/ymd40/air40/keymaps/w-colemak/keymap.c
+++ keyboards/ymdk/ymd40/air40/keymaps/w-colemak/keymap.c
@@ -18,6 +18,7 @@
 
 enum layer_names {
     _QWERTY,
+    _NM,
     _LOWER,
     _RAISE,
     _AD,
@@ -36,11 +37,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,    KC_LSFT, KC_ENT,  KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH,
         KC_CAPS, KC_LCTL, KC_LALT, KC_TAB,  L_UP,    KC_SPC,  KC_SPC,  L_DN,    KC_RALT, KC_DOWN, KC_UP,   KC_RGHT
     ),
+    [_NM] = LAYOUT_ortho_4x12(
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        KC_A,    KC_R,    KC_S,    KC_T,    _______, _______, _______, _______, KC_N,    KC_E,    KC_I,    KC_O,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+    ),
     [_LOWER] = LAYOUT_ortho_4x12(
         KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_TILD, KC_BSPC, KC_CIRC, KC_AMPR,    KC_ASTR,    KC_LPRN, KC_RPRN,
         KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_DEL,  KC_F6,   KC_UNDS,    KC_PLUS,    KC_LCBR, KC_RCBR,
         KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, KC_F12,  S(KC_NUHS), S(KC_NUBS), KC_HOME, KC_END,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______,    _______,    _______, _______
+        _______, _______, _______, _______, _______, TO(0),   TO(0),   _______, _______,    _______,    _______, _______
     ),
     [_RAISE] = LAYOUT_ortho_4x12(
         KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_GRV,  KC_BSPC, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
@@ -85,7 +92,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
     ),
     [_OS] = LAYOUT_ortho_4x12(
-        _______, _______, _______, _______, _______, _______, _______, TO(_AD), TO(_GL), TO(_ML), _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, TO(_AD), TO(_GL), TO(_ML), TO(_NM), _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
         KC_CAPS, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______, TO(0),   TO(0),   _______, _______, _______, _______, _______
@@ -111,6 +118,10 @@ bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
 
     if (layer_state_is(_ML)) {
         rgb_matrix_set_color(MOVEMENT_INDICATOR_LED, MOVEMENT_INDICATOR_COLOR);
+    }
+
+    if (layer_state_is(_NM)) {
+        rgb_matrix_set_color(NOMODS_INDICATOR_LED, NOMODS_INDICATOR_COLOR);
     }
 
     return false;
