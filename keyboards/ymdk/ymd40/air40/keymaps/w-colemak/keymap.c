5e115a35a3398fea6a8ee0a048f4b3f77b09785a
Marco Pacchialat <m.pacchialat@gmail.com>
2023-01-22 22:17:11 +0100

    Add numpad layer

--- keyboards/ymdk/ymd40/air40/keymaps/w-colemak/keymap.c
+++ keyboards/ymdk/ymd40/air40/keymaps/w-colemak/keymap.c
@@ -27,15 +27,16 @@ enum layer_names {
     _ML,
     _MH,
     _MT,
-    _OS
+    _OS,
+    _NP
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT_ortho_4x12(
-        KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,    KC_ESC,  KC_BSPC, KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN,
-        HOME_A,  HOME_R,  HOME_S,  HOME_T,  KC_G,    KC_TAB,  KC_QUOT, KC_M,    HOME_N,  HOME_E,  HOME_I,  HOME_O,
-        KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,    KC_LSFT, KC_ENT,  KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH,
-        KC_CAPS, KC_LCTL, KC_LALT, KC_TAB,  L_UP,    KC_SPC,  KC_SPC,  L_DN,    KC_RALT, KC_DOWN, KC_UP,   KC_RGHT
+        KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,    _______, _______, KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN,
+        HOME_A,  HOME_R,  HOME_S,  HOME_T,  KC_G,    _______, _______, KC_M,    HOME_N,  HOME_E,  HOME_I,  HOME_O,
+        KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,    _______, _______, KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH,
+        _______, _______, _______, _______, L_UP,    KC_SPC,  KC_SPC,  L_DN,    _______, _______, _______, _______
     ),
     [_NM] = LAYOUT_ortho_4x12(
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
@@ -44,9 +45,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
     ),
     [_LOWER] = LAYOUT_ortho_4x12(
-        KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_TILD, KC_BSPC, KC_CIRC, KC_AMPR,    KC_ASTR,    KC_LPRN, KC_RPRN,
-        KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_DEL,  KC_F6,   KC_UNDS,    KC_PLUS,    KC_LCBR, KC_RCBR,
-        KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, KC_F12,  S(KC_NUHS), S(KC_NUBS), KC_HOME, KC_END,
+        KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, _______, _______, KC_CIRC, KC_AMPR,    KC_ASTR,    KC_LPRN, KC_RPRN,
+        KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   _______, _______, KC_F6,   KC_UNDS,    KC_PLUS,    KC_LCBR, KC_RCBR,
+        KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  _______, _______, KC_F12,  S(KC_NUHS), S(KC_NUBS), KC_HOME, KC_END,
         _______, _______, _______, _______, _______, TO(0),   TO(0),   _______, _______,    _______,    _______, _______
     ),
     [_RAISE] = LAYOUT_ortho_4x12(
@@ -93,9 +94,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
     [_OS] = LAYOUT_ortho_4x12(
         _______, _______, _______, _______, _______, _______, _______, TO(_AD), TO(_GL), TO(_ML), TO(_NM), _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, TO(_NP),
         KC_CAPS, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______, TO(0),   TO(0),   _______, _______, _______, _______, _______
+    ),
+    [_NP] = LAYOUT_ortho_4x12(
+        KC_ESC,  _______, KC_UP,   _______, _______, _______, _______, KC_SLSH, KC_7,    KC_8,    KC_9,    KC_MINS,
+        KC_TAB,  KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, _______, KC_ASTR, KC_4,    KC_5,    KC_6,    KC_PLUS,
+        _______, _______, _______, _______, _______, _______, _______, KC_DOT,  KC_1,    KC_2,    KC_3,    KC_ENT,
+        _______, _______, _______, _______, _______, TO(0),   TO(0),   KC_0,    _______, _______, _______, _______
     )
 };
 
