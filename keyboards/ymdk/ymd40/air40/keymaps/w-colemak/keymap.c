bd0b3e019be5ce7d4c8d8268ad55b770d075249c
Marco Pacchialat <m.pacchialat@gmail.com>
2023-01-25 22:25:49 +0100

    move -=_+ keys

--- keyboards/ymdk/ymd40/air40/keymaps/w-colemak/keymap.c
+++ keyboards/ymdk/ymd40/air40/keymaps/w-colemak/keymap.c
@@ -45,14 +45,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______, _______, _______, _______, KC_SPC,  KC_SPC,  _______, _______, _______, _______, _______
     ),
     [_LOWER] = LAYOUT_ortho_4x12(
-        _______, _______, KC_UNDS, KC_PLUS, _______, _______, _______, KC_SLSH, KC_7,    KC_8,    KC_9,    KC_MINS,
-        KC_ESC,  KC_TAB,  KC_MINS, KC_EQL,  _______, _______, _______, KC_ASTR, KC_4,    KC_5,    KC_6,    KC_PLUS,
+        _______, _______, _______, _______, _______, _______, _______, KC_SLSH, KC_7,    KC_8,    KC_9,    KC_MINS,
+        KC_ESC,  KC_TAB,  _______, _______, _______, _______, _______, KC_ASTR, KC_4,    KC_5,    KC_6,    KC_PLUS,
         KC_DEL,  KC_RALT, CW_TOGG, _______, _______, _______, _______, KC_DOT,  KC_1,    KC_2,    KC_3,    KC_ENT,
         _______, _______, _______, _______, _______, TO(0),   TO(0),   KC_0,    _______, _______, _______, _______
     ),
     [_RAISE] = LAYOUT_ortho_4x12(
-        _______, _______, KC_LPRN, KC_RPRN, _______, _______, _______, KC_BSLS, KC_AMPR, KC_ASTR, _______, KC_GRV,
-        _______, _______, KC_LCBR, KC_RCBR, _______, _______, _______, KC_PIPE, KC_DLR,  KC_PERC, KC_CIRC, KC_QUOT,
+        KC_UNDS, KC_PLUS, KC_LPRN, KC_RPRN, _______, _______, _______, KC_BSLS, KC_AMPR, KC_ASTR, _______, KC_GRV,
+        KC_MINS, KC_EQL,  KC_LCBR, KC_RCBR, _______, _______, _______, KC_PIPE, KC_DLR,  KC_PERC, KC_CIRC, KC_QUOT,
         _______, KC_RALT, KC_LBRC, KC_RBRC, _______, _______, _______, KC_TILD, KC_EXLM, KC_AT,   KC_HASH, S(KC_QUOT),
         _______, _______, _______, _______, _______, OSL(_OS),OSL(_OS),_______, _______, _______, _______, _______
     ),
